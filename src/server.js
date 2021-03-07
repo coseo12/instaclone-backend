ca80af073a265194aa81047039bd9c9e8c177182
coseo12 <momo_12@naver.com>
2021-03-07 15:20:43 +0900

    backend setup

--- src/server.js
+++ src/server.js
@@ -1,45 +1,15 @@
-import { ApolloServer, gql } from 'apollo-server';
+require('dotenv').config();
+import { ApolloServer } from 'apollo-server';
+import schema from './schema';
 
-const typeDefs = gql`
-  type Movie {
-    id: Int
-    title: String
-    year: Int
-    createdAt: DateTime
-    updatedAt: DateTime
-  }
-  type Query {
-    movies: [Movie]
-    movie: Movie
-  }
-  type Mutation {
-    createMovie(title: String!): Boolean
-    deleteMovie(title: String!): Boolean
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => [{ id: 1, title: 'Hello', year: 2021 }],
-    movie: () => ({ id: 1, title: 'Hello', year: 2021 }),
-  },
-  Mutation: {
-    createMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
-    deleteMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
-  },
-};
+const PORT = process.env.PORT;
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 });
 
 server
-  .listen()
-  .then(() => console.log('Server is running on http://localhost:4000/'));
+  .listen(PORT)
+  .then(() =>
+    console.log(`🚀 Server is running on http://localhost:${PORT} ✅`)
+  );

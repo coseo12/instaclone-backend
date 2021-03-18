c373d4cf12bf004e3505412901836fb9f95a4cd3
coseo12 <momo_12@naver.com>
2021-03-18 16:23:55 +0900

    schema refactor

--- src/schema.js
+++ src/schema.js
@@ -2,7 +2,7 @@ import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 
-const loadedTypes = loadFilesSync(`${__dirname}/**/*.type_defs.js`);
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(
   `${__dirname}/**/*.{queries,mutations}.js`
 );

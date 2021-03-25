7494d515a90ef91b860a199a0b85aed2e9af7f6d
coseo12 <momo_12@naver.com>
2021-03-25 14:06:12 +0900

    protected resolvers refactor

--- src/users/users.utils.ts
+++ src/users/users.utils.ts
@@ -36,10 +36,15 @@ export const protectedResolver = (ourResolver: Resolver) => (
   info
 ) => {
   if (!context.loggedInUser) {
-    return {
-      ok: false,
-      error: 'Please log in to perform this action.',
-    };
+    const query = info.operation.operation === 'query';
+    if (query) {
+      return null;
+    } else {
+      return {
+        ok: false,
+        error: 'Please log in to perform this action.',
+      };
+    }
   }
   return ourResolver(root, args, context, info);
 };
